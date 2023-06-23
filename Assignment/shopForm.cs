5184b83dbb25e9591c9c7dc7128ffc8989423b66
Duy Bao <bao1322002@gmail.com>
2023-06-23 19:20:45 +0700

    fix: table generate

--- Assignment/shopForm.cs
+++ Assignment/shopForm.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using BUS;
+using DTO;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -15,31 +17,38 @@ namespace Assignment
     {
 
 
-        int tableCount = 20;
         private readonly IMapper _mapper;
+        private CoffeeTableBUS coffeeTableBUS;
         public shopForm(IMapper mapper)
         {
             InitializeComponent();
             this._mapper = mapper;
+            coffeeTableBUS = new CoffeeTableBUS(_mapper);
             FlowLayoutPanel flowLayoutPanel = new FlowLayoutPanel();
             flowLayoutPanel.Dock = DockStyle.Fill;
             flowLayoutPanel.AutoScroll = true;
             flowLayoutPanel.Padding = new Padding(10);
 
             pnlHome.Controls.Add(flowLayoutPanel);
-
-            for (int i = 1; i <= tableCount; i++)
+            var listTable = this.Tables();
+            foreach (TableCoffeeDTO item in listTable )
             {
-                Panel tablePanel = CreateTablePanel(i);
+                Panel tablePanel = CreateTablePanel(item.Id);
                 flowLayoutPanel.Controls.Add(tablePanel);
             }
 
         }
 
-        private Panel CreateTablePanel(int tableNumber)
+        private List<TableCoffeeDTO> Tables()
+        {
+            return coffeeTableBUS.getAll().ToList();
+        }
+
+
+        private Panel CreateTablePanel(string tableId)
         {
             Panel tablePanel = new Panel();
-            tablePanel.Name = "panelTable" + tableNumber;
+            tablePanel.Name = "panelTable" + tableId;
             tablePanel.BorderStyle = BorderStyle.FixedSingle;
             //tablePanel.BackColor = Color.LimeGreen;
             tablePanel.Size = new Size(120, 100);
@@ -53,14 +62,14 @@ namespace Assignment
             tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 20));
 
             PictureBox tablePictureBox = new PictureBox();
-            tablePictureBox.Name = "picTable" + tableNumber;
+            tablePictureBox.Name = "picTable" + tableId;
             tablePictureBox.ImageLocation = "C:\\Users\\duyba\\OneDrive\\Desktop\\video\\7113274.png";
             tablePictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
             tableLayoutPanel.Controls.Add(tablePictureBox, 0, 0);
 
             TextBox textBox = new TextBox();
-            textBox.Name = "txtTable" + tableNumber;
-            textBox.Text = "Bàn " + tableNumber;
+            textBox.Name = "txtTable" + tableId;
+            textBox.Text = "Bàn " + tableId;
             textBox.Enabled = false;
             textBox.Dock = DockStyle.Fill;
             tableLayoutPanel.Controls.Add(textBox, 0, 1);
